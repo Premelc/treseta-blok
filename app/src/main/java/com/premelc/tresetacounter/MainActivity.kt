f9cc68ad3ae84fcc84a04579b0ebddac8ff1fb21
Premelc <dominik.premelc33@gmail.com>
2024-03-17 19:52:25 +0100

    Add real ad unit id

--- app/src/main/java/com/premelc/tresetacounter/MainActivity.kt
+++ app/src/main/java/com/premelc/tresetacounter/MainActivity.kt
@@ -9,10 +9,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.RequestConfiguration
 import com.premelc.tresetacounter.navigation.NavigationHost
 import com.premelc.tresetacounter.ui.theme.TresetaBlokTheme
 import com.premelc.tresetacounter.utils.LocaleHelper.updateResources
+
+private val testDevices = listOf(
+    // S22 Ultra
+    "B428003909222CD446B1AD911C12D060",
+    // A52s
+    "E5FAE5B74F63A36F1E3F7831CAB8015F",
+    // for emulators
+    AdRequest.DEVICE_ID_EMULATOR
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,6 +39,9 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        MobileAds.setRequestConfiguration(
+            RequestConfiguration.Builder().setTestDeviceIds(testDevices).build()
+        )
         MobileAds.initialize(this) {}
     }
 
